79545ad106afab19366c000d10721099452a49fe
JothiYuvaraj <jothiyuvaraj29@gmail.com>
2024-03-28 00:04:52 +0530

    Added js

---
+++ product.js
@@ -0,0 +1,38 @@
+//selecting the Sidenavbar,MenuIcon
+var sidenav = document.getElementById("sidenav")
+var menuicon = document.getElementById("menuicon")
+var closenav = document.getElementById("closenav")
+
+menuicon.addEventListener("click",function(){
+    sidenav.style.right=0
+})
+
+closenav.addEventListener("click",function(){
+
+    sidenav.style.right="-50%"
+})
+// product search functionality
+
+var productcontainer = document.getElementById("productcontainer")
+var search = document.getElementById("search")
+var productlist = productcontainer.querySelectorAll("div")
+
+search.addEventListener("keyup",function()
+{
+    var enteredValue = event.target.value.toUppercase()
+
+
+for (count=0; count<productlist.length; count=count+1)
+{
+    var productname = productlist[count].querySelectorAll("h1").textContent
+    
+        if (productname.toUppercase().indexOf(enteredValue)<0)
+    {
+        productlist[count].style.display = "none"
+    }
+else{
+    productlist[count].style.display = "block"
+}
+
+}
+})
